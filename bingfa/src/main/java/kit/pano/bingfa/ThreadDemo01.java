c5d992939d7eca10825935e12543669ec6e9bca3
pano <362472407@qq.com>
2021-03-23 10:34:34 +0800

    add ReentrantLock

--- bingfa/src/main/java/kit/pano/bingfa/ThreadDemo01.java
+++ bingfa/src/main/java/kit/pano/bingfa/ThreadDemo01.java
@@ -1,5 +1,7 @@
 package kit.pano.bingfa;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 并发售票问题
  */
@@ -9,16 +11,29 @@ public class ThreadDemo01 implements Runnable {
 
     @Override
     public void run() {
-        while (true) {
+        while (tickets > 0) {
             sellTickets();
         }
     }
 
-    private synchronized void sellTickets() {
+//    //此处增加synchronized关键字，保证线程安全
+//    //但是通过加锁的方式实现线程安全的方式会导致性能降低
+//    private synchronized void sellTickets() {
+//        if (tickets > 0) {
+//            System.out.println(Thread.currentThread().getName() + tickets);
+//            tickets--;
+//        }
+//    }
+
+    //使用ReentrantLock加锁
+    ReentrantLock lock = new ReentrantLock();
+    private void sellTickets() {
+        lock.lock();
         if (tickets > 0) {
             System.out.println(Thread.currentThread().getName() + tickets);
             tickets--;
         }
+        lock.unlock();
     }
 
     public static void main(String[] args) {
